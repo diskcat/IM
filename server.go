072b806cbefda328e460049e55963001b12f5f63
prime <1450665930@qq.com>
2022-02-04 18:09:01 +0800

    消息群发功能

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"io"
 )
 
 type Server struct{
@@ -54,6 +55,27 @@ func (this *Server) Handler(conn net.Conn) {
 	this.mapLock.Unlock()
 	//将消息发送到message中,进行广播
 	this.BroadCast(user,"上线了")
+	//接收客户端发送的消息
+	go func(){
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Printf("/n cannot read control input:", err)
+				return
+			}
+
+			//接收传输的message
+			msg := string(buf[:n-1])
+			fmt.Printf(msg)
+			this.BroadCast(user, msg)
+		}
+	}()
 }
 
 //启动服务
